59cf3ea6c2ade805083ce1d4af1a01d5d873cd21
John Henry <john@iamjohnhenry.com>
2018-02-12 19:02:42 -0800

    Update publish script

--- .githooks/post-commit/1-publish.sh
+++ .githooks/post-commit/1-publish.sh
@@ -3,11 +3,11 @@ version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json
 
 if [ "$version" != "" ]; then
     branch=$(git rev-parse --abbrev-ref HEAD)
-    # if [ "$branch" == "master" ]; then
-    #     echo "Publishing..."  
-    #     npm publish
-    #     echo "\033[42mPublished v$version to NPM.\033[0m\n"
-    # fi
+    if [ "$branch" == "master" ]; then
+        echo "Publishing..."  
+        npm publish
+        echo "\033[42mPublished v$version to NPM.\033[0m\n"
+    fi
     echo "Pushing to remote repository $branch..."  
     git push origin $branch --tags
     echo "\033[42mPushed v$version to remote $branch.\033[0m\n"
