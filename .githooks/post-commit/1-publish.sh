94ca54f35b6dc3e357b8ef304723ea142d34ab7e
John Henry <john@iamjohnhenry.com>
2018-02-12 18:36:47 -0800

    update push to npm

--- .githooks/post-commit/1-publish.sh
+++ .githooks/post-commit/1-publish.sh
@@ -2,10 +2,10 @@
 version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
 
 if [ "$version" != "" ]; then
-    branch=$(git rev-parse --symbolic --abbrev-ref $1)
+    branch=$(git rev-parse --abbrev-ref HEAD)
     echo "Pushing to remote repository..."  
     #
-    #git push origin $branch
+    git push origin $branch
     if [branch == "master"]; then
         echo "Publishing..."  
         npm publish
