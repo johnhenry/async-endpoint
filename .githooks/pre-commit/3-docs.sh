f8ec05653bf85bad8d2798555f65af6bc9626827
John Henry <john@iamjohnhenry.com>
2018-02-13 18:37:50 -0800

    change where readme is generated + move logo

--- .githooks/pre-commit/3-docs.sh
+++ .githooks/pre-commit/3-docs.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 echo "\nCreating documentation...\n"
 npm run docs
+rm readme.md
 git add docs/api.md
-git add docs/readme.md
-cp docs/readme.md ./readme.md
 git add readme.md
 echo "\033[42mDocumentation successfully created!\033[0m\n"
