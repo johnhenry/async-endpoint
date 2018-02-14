b371bae52848fcd8250a7484b06c2772edd8ef4e
John Henry <john@iamjohnhenry.com>
2018-02-13 18:24:42 -0800

    Generate readme in top level (for npmjs.com)

--- .githooks/pre-commit/3-docs.sh
+++ .githooks/pre-commit/3-docs.sh
@@ -3,4 +3,6 @@ echo "\nCreating documentation...\n"
 npm run docs
 git add docs/api.md
 git add docs/readme.md
+cp docs/readme.md ./readme.md
+git add readme.md
 echo "\033[42mDocumentation successfully created!\033[0m\n"
