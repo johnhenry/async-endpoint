27f927d64856b2404d27dccdbf599ac15932d2f4
John Henry <john@iamjohnhenry.com>
2018-02-13 23:31:38 -0800

    Add reqdline shim for browser

--- mjs/input/console.mjs
+++ mjs/input/console.mjs
@@ -1,16 +1,26 @@
 //     
 /*global process */
 import readline from "readline";
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-const get = () =>
-  new Promise(resolve => {
-    //rl.pause();
-    //rl.question(">", answer => resolve(answer.slice(0, answer.length - 1)));
-    rl.question(">", answer => resolve(answer));
+
+readline.createInterface =
+  readline.createInterface ||
+  function() {
+    console.warn("readline has no browser equivalent");
+    return {
+      question() {
+        console.warn("readline has no browser equivalent");
+      }
+    };
+  }; //Shim included in browser
+
+const rl =
+  readline.createInterface &&
+  readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
   });
+const get = () =>
+  new Promise(resolve => rl.question(">", answer => resolve(answer)));
 export default async (...inputs                 ) => {
   while (true) {
     for (const input of inputs) {
